aca89ec0d2184610694719c79b35c3aa6237c369
olivialeahosb <olivia_osborne@biswashington.org>
2025-01-23 17:41:47 +0000

    here

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
+score = 0
+ 
+question = "what is the capital of england?"
+print(question)
+user_answer = input("your answer: ")
+score += 1
+if user_answer.lower() == "london":
+    print("Correct!")
+ 
+else:
+    print("oops! the correct answer is london")
+print(f"your score is: {score}")
+ 
+score = 0
+ 
+question = "what is knee in spanish?"
+print(question)
+user_answer = input("your answer: ")
+score += 1
+if user_answer.lower() == "la rodilla":
+    print("Correct!")
 
+else:
+    print("oops! the correct answer is la rodilla")
+print(f"your score is: {score}")
+
+score = 0
+ 
+question = "what is teeth in spanish?"
+print(question)
+user_answer = input("your answer: ")
+score += 1
+if user_answer.lower() == "los dientes":
+    print("Correct!")
+ 
+else:
+    print("oops! the correct answer is los dientes")
+print(f"your score is: {score}")
+score = 0
+
+question = "what is shoulder in spanish?"
+print(question)
+user_answer = input("your answer: ")
+score += 1
+if user_answer.lower() == "el hombro":
+    print("Correct!")
+
+else:
+    print("oops! the correct answer is el hombro")
+print(f"your score is: {score}")
